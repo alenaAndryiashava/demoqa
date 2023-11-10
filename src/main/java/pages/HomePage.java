1f882a0fcc8a55772ac3276418790dc58b3fc16b
Alena Andryiashava <alena.andryiashava@gmail.com>
2023-11-10 19:00:43 +0100

    update HomePage

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -6,54 +6,51 @@ import org.openqa.selenium.support.FindBy;
 import pages.AlertsFrameWindowsPages.AlertsPage;
 import pages.BookStore.BookStorePage;
 
-public class HomePage extends PageBase{
+public class HomePage extends PageBase {
     public HomePage(WebDriver driver) {
         super(driver);
     }
+
     @FindBy(xpath = "//*[@id=\"app\"]/div/div/div[2]/div/div[6]")
     WebElement bookStoreAppLink;
-
-
-
-    public BookStorePage openBookStoreApp(){
-        clickWithJSExecutor(bookStoreAppLink,0,300);
-
-        return new BookStorePage(driver);
-
-    }
-    @FindBy(xpath="//*[@id=\"app\"]/div/div/div[2]/div/div[3]/div/div[3]")
+    @FindBy(xpath = "//*[@id=\"app\"]/div/div/div[2]/div/div[3]/div/div[3]")
     WebElement alertsFrameWindowsLink;
-
-    public AlertsPage openAlertsFrameWindowsPage() {
-        click(alertsFrameWindowsLink);
-        return new AlertsPage(driver);
-
-    }
     @FindBy(xpath = "//*[@id=\"app\"]/div/div/div[2]/div/div[4]")
     WebElement widgetsLink;
-    public SideMenu openWidgetsPage() {
-        click(widgetsLink);
-        return new SideMenu(driver);
-    }
-
     @FindBy(xpath = " //body/div[@id='app']/div[1]/div[1]/div[2]/div[1]/div[1]")
     WebElement textBoxLink;
-
-    public SideMenu openElements() {
-        click(textBoxLink);
-        return new SideMenu(driver);
-    }
     @FindBy(xpath = "//*[@id=\"app\"]/div/div/div[2]/div/div[5]")
     WebElement interactionsPageLink;
-
-    public SideMenu openIntercations() {
-        click(interactionsPageLink);
-        return new SideMenu(driver);
-    }
     @FindBy(xpath = "//*[@id='app']/div[1]/div[1]/div[2]/div[1]/div[2]/div[1]/div[1]")
     WebElement formsLink;
-    public SideMenu openForms() {
+
+    public void openBookStoreApp() {
+        clickWithJSExecutor(bookStoreAppLink, 0, 300);
+        new BookStorePage(driver);
+    }
+
+    public void openAlertsFrameWindowsPage() {
+        click(alertsFrameWindowsLink);
+        new AlertsPage(driver);
+    }
+
+    public void openWidgetsPage() {
+        click(widgetsLink);
+        new SideMenu(driver);
+    }
+
+    public void openElements() {
+        click(textBoxLink);
+        new SideMenu(driver);
+    }
+
+    public void openInteractions() {
+        click(interactionsPageLink);
+        new SideMenu(driver);
+    }
+
+    public void openForms() {
         click(formsLink);
-        return new SideMenu(driver);
+        new SideMenu(driver);
     }
 }
